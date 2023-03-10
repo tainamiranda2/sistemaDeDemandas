1a7a070d944dce02d065863af703d02eabda985d
Tainá Miranda <tainaferreira0987@gmail.com>
2023-03-10 15:56:27 -0300

    página de login adiconada

--- src/router.jsx
+++ src/router.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 
 import {BrowserRouter,Routes, Route} from 'react-router-dom';
 import {Home}  from './views/home/Home';
+import {Login}  from './views/login/Login';
 
 export const Router=()=>{
     return(
         <BrowserRouter>
         <Routes>
           <Route path='/' element={<Home/>}/>
+          <Route path='/login' element={<Login/>}/>
+
           </Routes>
         </BrowserRouter>
     )
---
+++ src/views/login/Login.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export  const Login =()=>{
+return (
+<div>
+<h1>Olá, aqui é a paǵian de login</h1>
+</div>
+)
+}
